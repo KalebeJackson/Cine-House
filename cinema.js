c18f61c6cca56530feab719344f5101e1f474e75
Kalebe <santos.kalebe1234@gmail.com>
2021-08-26 21:05:34 -0300

    Incluindo arquivos

---
+++ cinema.js
@@ -0,0 +1,101 @@
+var filmes = require("./dataBase/Catalogo.json");
+
+
+var cinema = "CineHouse"
+var slogan = 'O melhor cinema da região'
+
+//console.log(cinema + ' // ' + slogan)
+
+//var filmes = [
+//    {
+ //       codigo: 1,
+//        titulo: 'Luca',
+//        duracao: 1.3,
+//        atores: ['Kalebe', 'Gioavanna', 'Leonardo'],
+//        anoLancamento: 2021,
+//        emCartaz: true
+//    },
+//]
+// [] == arrays
+// {} == objeto
+
+// var nomeDeFilmes = ['Luca', 'Moana', 'Mulan', 'Rei Leão', 'Cruella']
+
+// filmes[x].emCartaz = true || false
+
+function adicionarFilme(
+    codigo, 
+    titulo,
+    duracao,
+    atores,
+    anoLancamento,
+    emCartaz) {
+
+    filmes.push(
+        {
+            codigo: codigo, 
+            titulo: titulo,
+            duracao: duracao,
+            atores: atores,
+            anoLancamento: anoLancamento,
+            emCartaz: emCartaz
+        }
+    )
+}
+
+function buscarFilme(codigo) {
+    const filmeEncontrado = filmes.find(filme => filme.codigo == codigo)
+    // const filmeEncontrado = filmes.find( 
+    //     function(filme) { 
+    //         return filme.codigo == codigo
+    //     }
+    // )
+    //console.log('O filme encontrado foi "' + filmeEncontrado.titulo + '"')
+}
+
+function alterarStatusEmCartaz(codigo, buscarFilmeCallback){
+    const filmeEncontrado = buscarFilmeCallback(codigo)
+    filmeEncontrado.emCartaz = filmeEncontrado.emCartaz ? false : true
+    // const posicao = filmes.indexOf(filmeEncontrado)
+    // filmes[posicao].emCartaz = !filmes[posicao].emCartaz
+}
+
+// // adicionando filmes
+adicionarFilme(2, 'Raya e o Último Dragão', 2, ['Gemma', 'Sandra', 'Alan'], 2021, true)
+adicionarFilme(3, 'Coração de Dragão', 2, ['Gemma', 'Sandra', 'Alan'], 2021, true)
+
+// Lista filmes
+//console.log('Lista de filmes')
+//console.log('--------------')
+//console.log(filmes)
+
+
+/// buscar filmes
+// console.log('Buscar filme')
+// console.log('--------------')
+// buscarFilme(3)
+
+// /// alterar o stats de um filme
+//console.log('Alterar filme em cartaz')
+//console.log('--------------')
+//alterarStatusEmCartaz(3)
+
+
+//console.log(filmes)
+var titulo = filmes[1].titulo
+//console.log(titulo)//string
+//console.log(titulo.length)//tamanho da string
+//console.log(titulo[5])//caractere
+//console.log(titulo.indexOf('e'))//saber se um fragmento de texto existe dentro de uma string
+//console.log(titulo.slice(9,15))// recorta informações de uma string
+
+for (const filme of filmes) {
+    console.log(filme.titulo)
+}
+
+filmes.forEach(function(filme, index){
+    //console.log(index + ' - ' + filme.titulo)// imprime o que determinamos para eles por meio de uma função
+})
+
+let listaDeTitulos = filmes.map( filme => filme.titulo)
+//console.log(titulo)
